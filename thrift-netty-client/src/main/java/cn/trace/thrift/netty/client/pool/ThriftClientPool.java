038f73647e4ebc560d5a2c0e39fa2c34a255a2db
tracenow <liuyuctrlz@gmail.com>
2016-12-26 16:01:35 +0800

    add multi thread client examples

--- thrift-netty-client/src/main/java/cn/trace/thrift/netty/client/pool/ThriftClientPool.java
+++ thrift-netty-client/src/main/java/cn/trace/thrift/netty/client/pool/ThriftClientPool.java
@@ -5,7 +5,6 @@ import java.net.InetSocketAddress;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 
 import cn.trace.thrift.netty.common.pool.Pool;
-import cn.trace.thrift.netty.common.pool.PoolException;
 
 import com.facebook.swift.service.ThriftClientManager;
 
@@ -22,25 +21,6 @@ public class ThriftClientPool<T extends AutoCloseable> extends Pool<T> {
 		this.internalPool = new GenericObjectPool<T>(
 		        new ThriftClientFactory<T>(clientManager, socketAddress, type, poolConfig),
 		        poolConfig.getPoolConfig());
-	}
-
-	public void returnBrokenResource(T resource) {
-		if (resource != null)
-			returnBrokenResourceObject(resource);
-	}
-
-	public void returnResource(T resource) {
-		if (resource != null)
-			try {
-				returnResourceObject(resource);
-			} catch (Exception e) {
-				returnBrokenResource(resource);
-				throw new PoolException("Could not return the resource to the pool", e);
-			}
-	}
-
-    public T getResource() {
-		return super.getResource();
 	}
 
 	@Override
--- thrift-netty-examples/src/main/java/cn/trace/thrift/netty/examples/client/EchoClient.java
+++ thrift-netty-examples/src/main/java/cn/trace/thrift/netty/examples/client/EchoClient.java
@@ -1,9 +1,10 @@
 package cn.trace.thrift.netty.examples.client;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.CountDownLatch;
 
-import cn.trace.thrift.netty.client.pool.ThriftClientPool;
 import cn.trace.thrift.netty.client.pool.ThriftClientConfig;
+import cn.trace.thrift.netty.client.pool.ThriftClientPool;
 import cn.trace.thrift.netty.common.pool.Pool;
 import cn.trace.thrift.netty.examples.api.Echo;
 
@@ -21,17 +22,30 @@ public class EchoClient {
 	public static void main(String[] args) throws Exception {
 	    ThriftClientConfig config = new ThriftClientConfig();
 		InetSocketAddress socketAddress = new InetSocketAddress("localhost", 8080);
-		Pool<Echo> pool = new ThriftClientPool<Echo>(config, socketAddress, Echo.class);
-	    Echo client = null;
-	    try {
-	    	client = pool.getResource();
-	    	System.out.println(client.echo("Hello World"));
-	    } catch(Throwable t) {
-	        pool.returnBrokenResource(client);
-	        client = null;
-	    } finally {
-	        pool.returnResource(client);
-	    }
+		int count = 100;
+		final Pool<Echo> pool = new ThriftClientPool<Echo>(config, socketAddress, Echo.class);
+		final CountDownLatch latch = new CountDownLatch(count);
+		for(int i = 0; i < count; i++) {
+		    new Thread(new Runnable(){
+
+                @Override
+                public void run() {
+                    Echo client = null;
+                    try {
+                        client = pool.getResource();
+                        System.out.println(client.echo("Hello World"));
+                    } catch(Throwable t) {
+                        pool.returnBrokenResource(client);
+                        client = null;
+                    } finally {
+                        pool.returnResource(client);
+                        latch.countDown();
+                    }
+                }
+		        
+		    }).start();
+		}
+		latch.await();
 	    pool.close();
 	}
 
